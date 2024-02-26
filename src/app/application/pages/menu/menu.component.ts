f2e4b487dc96ea3a2bc6eec04272b8f98ce4a36b
SebastianSantis <sebastiandasanhe.8712@gmail.com>
2024-02-25 22:26:43 -0500

    Integrando menu-hamburguesa con page menu

--- src/app/application/pages/menu/menu.component.ts
+++ src/app/application/pages/menu/menu.component.ts
@@ -1,9 +1,14 @@
+// First level importation
 import { Component } from '@angular/core';
+// Second level importation
+import { HamburgerMenuComponent } from '../../components/main/hamburger-menu/hamburger-menu.component';
+// Third level importation
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [HamburgerMenuComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
