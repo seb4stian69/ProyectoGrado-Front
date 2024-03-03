efe26bb0ab7e641e85763da64f4abe7ed026d467
SebastianSantis <sebastiandasanhe.8712@gmail.com>
2024-03-03 14:49:02 -0500

    Agregando configuraciones iniciales

--- src/app/application/pages/testview/testview.component.ts
+++ src/app/application/pages/testview/testview.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+// First level importation
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+// Second level importation
+import { HamburgerMenuComponent } from '../../components/main/hamburger-menu/hamburger-menu.component';
+import { InformationCardComponent } from '../../components/main/information-card/information-card.component';
+// Third level importation
 
 @Component({
   selector: 'app-testview',
   standalone: true,
-  imports: [],
+  imports: [HamburgerMenuComponent, InformationCardComponent],
   templateUrl: './testview.component.html',
   styleUrl: './testview.component.scss'
 })
 export class TestviewComponent {
+
+  router: Router = inject(Router);
+
+  toggleMenu() {
+    const div:HTMLElement = document.querySelector('.cardGrid')!;
+    div.classList.toggle('original');
+  }
+
+  goToView(url:string){
+    this.router.navigate([url]);
+  }
 
 }
