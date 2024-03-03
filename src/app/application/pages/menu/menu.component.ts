7da0fd21bf1e5998c9544dc21c87f8eb2d5766af
SebastianSantis <sebastiandasanhe.8712@gmail.com>
2024-03-03 14:30:27 -0500

    Modificando flujo de datos entre page y components

--- src/app/application/pages/menu/menu.component.ts
+++ src/app/application/pages/menu/menu.component.ts
@@ -1,8 +1,10 @@
 // First level importation
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 // Second level importation
 import { HamburgerMenuComponent } from '../../components/main/hamburger-menu/hamburger-menu.component';
 import { TestCardComponent } from '../../components/main/test-card/test-card.component';
+import { TestCardData } from '../../../interfaces/application-data/testcarddata';
 // Third level importation
 
 
@@ -15,11 +17,28 @@ import { TestCardComponent } from '../../components/main/test-card/test-card.com
 })
 export class MenuComponent {
 
-  toggleMenu() {
+  router: Router = inject(Router);
 
+  testCardList: TestCardData[] = [
+    {
+      title: "Test de pensamiento critico",
+      srcImg: "ImgTest.png",
+      secondTitle: "Cuestionario de Reflexion Critica",
+      testID: "321"
+    }
+  ]
+
+  toggleMenu() {
     const div:HTMLElement = document.querySelector('.cardGrid')!;
     div.classList.toggle('original');
-    
+  }
+
+  goToTest(id:string){
+    this.router.navigate([`/tomakexam/123/${id}`]);
+  }
+
+  goToView(url:string){
+    this.router.navigate([url]);
   }
 
 }
